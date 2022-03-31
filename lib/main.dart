741b11165950a14773b547d329287267813fbe69
Fatihcan Uslu <19360859048@ogrenci.btu.edu.tr>
2022-03-31 22:11:42 +0300

    commit

---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/eduswappapp/generatedmainpagewidget/GeneratedMainPageWidget.dart';
+import 'package:flutterapp/eduswappapp/generatedservicecataloguewidget/GeneratedServiceCatalogueWidget.dart';
+import 'package:flutterapp/eduswappapp/generatedservicecategorypagewidget/GeneratedServiceCategoryPageWidget.dart';
+import 'package:flutterapp/eduswappapp/generatedframe43widget1/GeneratedFrame43Widget1.dart';
+import 'package:flutterapp/eduswappapp/generatedgroup98widget/GeneratedGroup98Widget.dart';
+import 'package:flutterapp/eduswappapp/generatedcontactspagewidget/GeneratedContactsPageWidget.dart';
+import 'package:flutterapp/eduswappapp/generatedmessagepagewidget/GeneratedMessagePageWidget.dart';
+import 'package:flutterapp/eduswappapp/generatedsatpagewidget/GeneratedSatPageWidget.dart';
+import 'package:flutterapp/eduswappapp/generatedilanurunwidget/GeneratedIlanUrunWidget.dart';
+import 'package:flutterapp/eduswappapp/generatedilanhizmetwidget/GeneratedIlanHizmetWidget.dart';
+import 'package:flutterapp/eduswappapp/generatedprofilepagewidget/GeneratedProfilePageWidget.dart';
+import 'package:flutterapp/eduswappapp/generatedlearnwidget/GeneratedLearnWidget.dart';
+import 'package:flutterapp/eduswappapp/generatedhubwidget/GeneratedHubWidget.dart';
+import 'package:flutterapp/eduswappapp/generatedvideowidget/GeneratedVideoWidget.dart';
+import 'package:flutterapp/eduswappapp/generatedgroup97widget/GeneratedGroup97Widget.dart';
+import 'package:flutterapp/eduswappapp/generatedexplorewidget/GeneratedExploreWidget.dart';
+import 'package:flutterapp/eduswappapp/generatedmatchedwidget/GeneratedMATCHEDWidget.dart';
+import 'package:flutterapp/eduswappapp/generatedlistboxtitlewidget2/GeneratedListBoxtitleWidget2.dart';
+import 'package:flutterapp/eduswappapp/generatedplaceholdertextwidget2/GeneratedPlaceholderTextWidget2.dart';
+import 'package:flutterapp/eduswappapp/generatedhoverstatewidget/GeneratedHoverStateWidget.dart';
+import 'package:flutterapp/eduswappapp/generatedlistboxcomponentwidget2/GeneratedListboxComponentWidget2.dart';
+import 'package:flutterapp/eduswappapp/generatedrectangle3widget2/GeneratedRectangle3Widget2.dart';
+import 'package:flutterapp/eduswappapp/generatedchangedataherewidget/GeneratedChangedatahereWidget.dart';
+import 'package:flutterapp/eduswappapp/generatedgroupwidget30/GeneratedGroupWidget30.dart';
+import 'package:flutterapp/eduswappapp/generatedlistboxtitlewidget3/GeneratedListBoxtitleWidget3.dart';
+import 'package:flutterapp/eduswappapp/generatedplaceholdertextwidget3/GeneratedPlaceholderTextWidget3.dart';
+import 'package:flutterapp/eduswappapp/generatedhoverstatewidget1/GeneratedHoverStateWidget1.dart';
+import 'package:flutterapp/eduswappapp/generatedlistboxcomponentwidget3/GeneratedListboxComponentWidget3.dart';
+import 'package:flutterapp/eduswappapp/generatedrectangle4widget/GeneratedRectangle4Widget.dart';
+import 'package:flutterapp/eduswappapp/generatedchangedataherewidget1/GeneratedChangedatahereWidget1.dart';
+import 'package:flutterapp/eduswappapp/generatedgroupwidget31/GeneratedGroupWidget31.dart';
+import 'package:flutterapp/eduswappapp/generatedexplorewidget2/GeneratedExploreWidget2.dart';
+import 'package:flutterapp/eduswappapp/generatedexplorewidget3/GeneratedExploreWidget3.dart';
+import 'package:flutterapp/eduswappapp/generatedgroup73widget1/GeneratedGroup73Widget1.dart';
+import 'package:flutterapp/eduswappapp/generatedexplorewidget4/GeneratedExploreWidget4.dart';
+import 'package:flutterapp/eduswappapp/generatedellipse48widget7/GeneratedEllipse48Widget7.dart';
+import 'package:flutterapp/eduswappapp/generatedgroup72widget7/GeneratedGroup72Widget7.dart';
+import 'package:flutterapp/eduswappapp/generatedellipse47widget7/GeneratedEllipse47Widget7.dart';
+import 'package:flutterapp/eduswappapp/generatedstatusbarwidget7/GeneratedStatusBarWidget7.dart';
+import 'package:flutterapp/eduswappapp/generatedprofilewidget/GeneratedPROFILEWidget.dart';
+
+void main() {
+  runApp(eduswappApp());
+}
+
+class eduswappApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedExploreWidget',
+      routes: {
+        '/GeneratedMainPageWidget': (context) => GeneratedMainPageWidget(),
+        '/GeneratedServiceCatalogueWidget': (context) =>
+            GeneratedServiceCatalogueWidget(),
+        '/GeneratedServiceCategoryPageWidget': (context) =>
+            GeneratedServiceCategoryPageWidget(),
+        '/GeneratedFrame43Widget1': (context) => GeneratedFrame43Widget1(),
+        '/GeneratedGroup98Widget': (context) => GeneratedGroup98Widget(),
+        '/GeneratedContactsPageWidget': (context) =>
+            GeneratedContactsPageWidget(),
+        '/GeneratedMessagePageWidget': (context) =>
+            GeneratedMessagePageWidget(),
+        '/GeneratedSatPageWidget': (context) => GeneratedSatPageWidget(),
+        '/GeneratedIlanUrunWidget': (context) => GeneratedIlanUrunWidget(),
+        '/GeneratedIlanHizmetWidget': (context) => GeneratedIlanHizmetWidget(),
+        '/GeneratedProfilePageWidget': (context) =>
+            GeneratedProfilePageWidget(),
+        '/GeneratedLearnWidget': (context) => GeneratedLearnWidget(),
+        '/GeneratedHubWidget': (context) => GeneratedHubWidget(),
+        '/GeneratedVideoWidget': (context) => GeneratedVideoWidget(),
+        '/GeneratedGroup97Widget': (context) => GeneratedGroup97Widget(),
+        '/GeneratedExploreWidget': (context) => GeneratedExploreWidget(),
+        '/GeneratedMATCHEDWidget': (context) => GeneratedMATCHEDWidget(),
+        '/GeneratedListBoxtitleWidget2': (context) =>
+            GeneratedListBoxtitleWidget2(),
+        '/GeneratedPlaceholderTextWidget2': (context) =>
+            GeneratedPlaceholderTextWidget2(),
+        '/GeneratedHoverStateWidget': (context) => GeneratedHoverStateWidget(),
+        '/GeneratedListboxComponentWidget2': (context) =>
+            GeneratedListboxComponentWidget2(),
+        '/GeneratedRectangle3Widget2': (context) =>
+            GeneratedRectangle3Widget2(),
+        '/GeneratedChangedatahereWidget': (context) =>
+            GeneratedChangedatahereWidget(),
+        '/GeneratedGroupWidget30': (context) => GeneratedGroupWidget30(),
+        '/GeneratedListBoxtitleWidget3': (context) =>
+            GeneratedListBoxtitleWidget3(),
+        '/GeneratedPlaceholderTextWidget3': (context) =>
+            GeneratedPlaceholderTextWidget3(),
+        '/GeneratedHoverStateWidget1': (context) =>
+            GeneratedHoverStateWidget1(),
+        '/GeneratedListboxComponentWidget3': (context) =>
+            GeneratedListboxComponentWidget3(),
+        '/GeneratedRectangle4Widget': (context) => GeneratedRectangle4Widget(),
+        '/GeneratedChangedatahereWidget1': (context) =>
+            GeneratedChangedatahereWidget1(),
+        '/GeneratedGroupWidget31': (context) => GeneratedGroupWidget31(),
+        '/GeneratedExploreWidget2': (context) => GeneratedExploreWidget2(),
+        '/GeneratedExploreWidget3': (context) => GeneratedExploreWidget3(),
+        '/GeneratedGroup73Widget1': (context) => GeneratedGroup73Widget1(),
+        '/GeneratedExploreWidget4': (context) => GeneratedExploreWidget4(),
+        '/GeneratedEllipse48Widget7': (context) => GeneratedEllipse48Widget7(),
+        '/GeneratedGroup72Widget7': (context) => GeneratedGroup72Widget7(),
+        '/GeneratedEllipse47Widget7': (context) => GeneratedEllipse47Widget7(),
+        '/GeneratedStatusBarWidget7': (context) => GeneratedStatusBarWidget7(),
+        '/GeneratedPROFILEWidget': (context) => GeneratedPROFILEWidget(),
+      },
+    );
+  }
+}
